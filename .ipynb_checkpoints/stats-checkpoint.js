f8854c94afb84e57b49ffb402e1d0d13875ee1a2
Evan Burchard <Evan@jades-mbp.myfiosgateway.com>
2023-10-27 13:50:39 -0400

    load more files into pandas

--- .ipynb_checkpoints/stats-checkpoint.js
+++ .ipynb_checkpoints/stats-checkpoint.js
@@ -26,20 +26,3 @@ if (!fs.existsSync(analytics_directory)) {
 fs.writeFile(analytics_directory + "/" + game_name.split('/').pop().replace('.slp', '.json'), JSON.stringify(game_stats), (error) => {
     if (error) throw error;
 });
-
-// // Get game settings – stage, characters, etc
-// const settings = game.getSettings();
-// console.log(settings);
-
-// // Get metadata - start time, platform played on, etc
-// const metadata = game.getMetadata();
-// console.log(metadata);
-
-// // Get computed stats - openings / kill, conversions, etc
-// const stats = game.getStats();
-// console.log(stats);
-
-// // Get frames – animation state, inputs, etc
-// // This is used to compute your own stats or get more frame-specific info (advanced)
-// const frames = game.getFrames();
-// console.log(frames[0].players); // Print frame when timer starts counting down
---
+++ README.md
@@ -0,0 +1,12 @@
+# About
+Lookup tables characters.json, moves.json, and stages.json are from https://github.com/project-slippi/slippi-js/blob/master/src/melee
+
+characters.json doesn't have default colors. 
+
+# TODO
+
+* Add (prepend to array) default colors for characters
+* Rework dataframe with lookup tables
+* Form subset of data record
+* Load multiple games into dataframe
+* Compute aggregate stats
